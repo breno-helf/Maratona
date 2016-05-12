003b85ea4fe0d7786a85bb1c406a4776a5324880
breno-helf <breno.moura@hotmail.com>
2016-05-12 07:59:42 -0300

    Header adicionado

---
+++ Done/header.cpp
@@ -0,0 +1,19 @@
+//This code was made by Breno Moura, Codeforces Handle: Brelf
+//From University of São Paulo - USP
+//If you are trying to hack me I wish you can get it, Good Luck :D
+//However if we are in the last five minutes of the contest, GO AWAY
+#include<bits/stdc++.h>
+using namespace std;
+
+#define debug(args...) fprintf(stderr,args)
+#define pb push_back
+
+typedef long long ll;
+typedef pair<int,int> pii;
+
+const int MAXN=
+const int INF=0x3f3f3f3f;
+
+
+
+
