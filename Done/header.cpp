1a4c9640c10f0e264f49b075979ce5774c616662
breno-helf <breno.moura@hotmail.com>
2016-06-01 17:00:45 -0300

    changing header

--- Done/header.cpp
+++ Done/header.cpp
@@ -1,19 +1,20 @@
 //This code was made by Breno Moura, Codeforces Handle: Brelf
 //From University of São Paulo - USP
 //If you are trying to hack me I wish you can get it, Good Luck :D
-//However if we are in the last five minutes of the contest, GO AWAY
 #include<bits/stdc++.h>
 using namespace std;
 
 #define debug(args...) fprintf(stderr,args)
 #define pb push_back
+#define mp make_pair
 
 typedef long long ll;
 typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
 
 const int MAXN=
 const int INF=0x3f3f3f3f;
-
+const int MOD=1000000007;
 
 
 
